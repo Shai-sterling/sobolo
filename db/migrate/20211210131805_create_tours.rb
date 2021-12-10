8b2f44dc2116f5f8981b0dcc6af0809c4baf1a38
shaibusterling <pattiminka@gmail.com>
2021-12-10 14:27:08 +0100

    Adding tour model with attributes

---
+++ db/migrate/20211210131805_create_tours.rb
@@ -0,0 +1,17 @@
+class CreateTours < ActiveRecord::Migration[6.1]
+  def change
+    create_table :tours do |t|
+      t.string "name", null: false
+      t.decimal "price", precision: 5, scale: 2, null: false
+      t.integer "duration"
+      t.string "accomodation"
+      t.string "meals"
+      t.string "entry_tickets"
+      t.string "pick_up"
+      t.string "transport"
+
+
+      t.timestamps
+    end
+  end
+end
