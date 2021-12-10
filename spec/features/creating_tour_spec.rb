de37254d21fbf85191a2f397a1a5d9c55457791b
shaibusterling <pattiminka@gmail.com>
2021-12-10 14:04:56 +0100

    Adding tours controller

--- spec/features/creating_tour_spec.rb
+++ spec/features/creating_tour_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe "Creating a Tour", type: :system do
+RSpec.describe "Creating a Tour", type: :feature do
   
   scenario "Valid inputs" do
     visit new_tour_path
