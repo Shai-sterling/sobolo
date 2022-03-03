87a02e21358f246f613fe5f4dfe6c4c75ad88cfb
shasterling <dennissremsai>
2022-03-03 18:11:04 +0100

    Adding set tour method to sites

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -1,41 +1,37 @@
 class SitesController < ApplicationController
 
     before_action :authenticate_user!
-
+    before_action :set_tour, only: %i[:create, :edit, :update, :destroy]
 
     def create   
-        @tour = Tour.find(params[:tour_id])
         @site = @tour.sites.create(site_params)
 
         if @site.save
             redirect_to @tour
         end
-
     end
 
     def edit 
-        @tour = Tour.find(params[:tour_id])
         @site = @tour.sites.find(params[:id])
     end
 
     def update
-        @tour = Tour.find(params[:tour_id])
         @site = @tour.sites.find(params[:id])
         @site.update(site_params)
         redirect_to @tour
     end
 
     def destroy
-        @tour = Tour.find(params[:tour_id])
         @site = @tour.sites.find(params[:id])
         @site.destroy   
         redirect_to @tour
     end
 
-
-
-
     private
+
+    def set_tour
+        @tour = Tour.find(params[:tour_id])
+    end
 
     def site_params
         params.require(:site).permit(:name, :description)
