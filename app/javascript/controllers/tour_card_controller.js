c9b585a9c5b4782f1e03ebd9964c9edfbe89ffe8
Shai-sterling <pattiminka@gmail.com>
2022-03-24 12:48:55 +0100

    Adding close times to side nav

--- app/javascript/controllers/tour_card_controller.js
+++ app/javascript/controllers/tour_card_controller.js
@@ -5,20 +5,17 @@ export default class extends Controller {
 
     mouse(event) {
         for (let i = 0; i < this.cardTargets.length; i++) {
+            
             const element = this.cardTargets[i];
 
             element.addEventListener("mouseenter", function(){ 
-                console.log("tour clicked"); 
-                console.log(this);
                 this.style.transform = "scale(1.04)"; 
 
             });
 
             element.addEventListener("mouseleave", function() {
-                console.log("event removed!!");
                 this.style.color = ""  
                 this.style.transform = ""
-
             })
         }
     }
