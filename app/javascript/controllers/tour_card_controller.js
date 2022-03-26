9c268c8ad0da6ba04e24f435235fb8b1de07e3b5
Shai-sterling <pattiminka@gmail.com>
2022-03-26 18:33:23 +0100

    Adding Box shadow to cards

--- app/javascript/controllers/tour_card_controller.js
+++ app/javascript/controllers/tour_card_controller.js
@@ -10,12 +10,14 @@ export default class extends Controller {
 
             element.addEventListener("mouseenter", function(){ 
                 this.style.transform = "scale(1.04)"; 
+                this.style.boxShadow = "5px 10px 18px #8888888a"; 
 
             });
 
             element.addEventListener("mouseleave", function() {
                 this.style.color = ""  
                 this.style.transform = ""
+                this.style.boxShadow = ""
             })
         }
     }
